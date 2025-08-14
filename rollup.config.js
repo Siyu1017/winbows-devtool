12b5a0aa51db5b53579ec4ff28b5fa64272e9a94
Siyu1017 <adibao1111@gmail.com>
2025-08-14 15:42:38 +0800

    release: v1.0.0

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+const terser = require("@rollup/plugin-terser");
+const pkg = require("./package.json");
+const postcss = require("rollup-plugin-postcss");
+
+module.exports = [{
+    input: `src/index.js`,
+    output: [
+        {
+            file: `dist/index.js`,
+            format: 'esm',
+            banner: `/*!
+ * winbows-devtool v${pkg.version}
+ * Copyright (c) Siyu1017 ${new Date().getFullYear()}
+ * Github : Siyu1017/winbows-devtool
+ */`
+        }
+    ],
+    plugins: [
+        terser(),
+        postcss({
+            module: true,
+            extract: true,
+            minimize: true
+        })
+    ]
+}];
